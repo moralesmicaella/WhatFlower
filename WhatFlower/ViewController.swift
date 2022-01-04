bf4e7bf8951a20893062837a23d2b773375ad49e
Micaella Morales <morales.micaella@gmail.com>
2022-01-04 04:52:23 -0800

    Integrate CoreML and Vision

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
     
@@ -26,13 +28,41 @@ class ViewController: UIViewController {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func detect(flowerImage: CIImage) {
+        let configuration = MLModelConfiguration()
+        guard let flowerClassifier = try? FlowerClassifier(configuration: configuration) else {
+            fatalError("App failed to create a flower classifier model instance.")
+        }
+        guard let model = try? VNCoreMLModel(for: flowerClassifier.model) else {
+            fatalError("App failed to create a VNCoreMLModel instance.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, erroe) in
+            guard let result = request.results?.first as? VNClassificationObservation else {
+                fatalError("App failed to complete classification.")
+            }
+            
+            self.title = result.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage, options: [:])
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[.editedImage] as? UIImage {
-            flowerImageView.image = image
-            print("set image")
+        if let userPickedImage = info[.editedImage] as? UIImage {
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("App failed to convert image to CIImage.")
+            }
+            detect(flowerImage: convertedCIImage)
+            flowerImageView.image = userPickedImage
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
